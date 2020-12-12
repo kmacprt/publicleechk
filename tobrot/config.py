b079a84c71f8f33f791ff1fadec76e4d978b09ed
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 06:38:20 +0530

    Update and rename sample_config.py to config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -43,7 +43,7 @@ class Config:
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = get_config(
         "DEF_THUMB_NAIL_VID_S",
-        "https://telegra.ph/file/8b973b270f4f380a427b1.png"
+        "https://telegra.ph/file/56c895014345edcbd1f63.png"
     )
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = int(get_config(
